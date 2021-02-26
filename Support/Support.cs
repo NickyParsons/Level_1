d1ecc613f4f8d6569809474aef183c652774f337
NickyParsons <79032032193@ya.ru>
2021-02-26 14:47:07 +0300

    Lesson 4 commit 1

---
+++ Support/Support.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Support
+{
+    public class Support
+    {
+        public static void Next()
+        {
+            //Переход к следующему заданию
+            Console.WriteLine("\nНажмите Enter для продолжения...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
