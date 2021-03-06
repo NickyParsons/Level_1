314445fef957d2b5709311f51998ddffc57f149d
NickyParsons <79032032193@ya.ru>
2021-03-06 11:39:13 +0300

    Lesson 5 commit 1

--- Support/Support.cs
+++ Support/Support.cs
@@ -2,14 +2,29 @@
 
 namespace Support
 {
-    public class Support
+    static public class Support
     {
+        //Переход к следующему заданию
         public static void Next()
         {
-            //Переход к следующему заданию
             Console.WriteLine("\nНажмите Enter для продолжения...");
             Console.ReadLine();
             Console.Clear();
+        }
+
+        //Распечатать элементы массива
+        public static void PrintAray<T>(T[] array)
+        {
+            Console.Write("Массив: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.Write(";");
         }
     }
 }
